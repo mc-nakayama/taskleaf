338cfabbe256518683c01c2f232b3655ae8f38c7
mc-nakayama <michitoshi.nakayama@gmail.com>
2021-02-01 00:46:16 +0900

    RSpec

--- test/controllers/seesons_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SeesonsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get seesons_new_url
-    assert_response :success
-  end
-
-end
